3ff492ad00da7c9fc3b65336f92fcfe57cfa6447
nhannv-zigexn <nhannv@zigexn.vn>
2024-06-02 23:26:57 +0700

    Setup vite and js, sass done

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
-import './bootstrap';
+// Check and set Theme Mode
+if (document.documentElement) {
+    const defaultThemeMode = "system";
+    let themeMode = localStorage.getItem("theme_mode_value");
+    if (themeMode === null) {
+        if (defaultThemeMode === "system") {
+            themeMode = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+        } else {
+            themeMode = defaultThemeMode;
+        }
+    }
+    console.log("Dark THeme");
+    if (!document.body.classList.contains(themeMode)) {
+        document.body.classList.add(themeMode);
+    }
+}
+
