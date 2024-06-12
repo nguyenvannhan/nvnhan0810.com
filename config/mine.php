ce5e7663554e9ac5cb8353dbfbc3387bcad54b77
Nguyen Van Nhan <nguyenvannhan0810@gmail.com>
2024-06-12 22:38:32 +0700

    Update note url page

--- config/mine.php
+++ config/mine.php
@@ -4,6 +4,6 @@ return [
     'links' => [
         'github' => 'https://github.com/nguyenvannhan',
         'linkedin' => 'https://www.linkedin.com/in/nguyen-van-nhan-754052148/',
-        'blog' => 'https://blog.nvnhan0810.com',
+        'blog' => 'https://notes.nvnhan0810.com',
     ],
 ];
